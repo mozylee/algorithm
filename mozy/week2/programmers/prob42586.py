ce481f66b4e5a1361d06d2a29bfd4d71e7c5ac9f
Mozy Lee <whyalwaysmeyy@gmail.com>
2020-08-09 01:01:12 +0900

    Refactor week2/programmers/prob42586.py
    더 나은 방식으로 리팩토링하여 다시 커밋

--- mozy/week2/programmers/prob42586.py
+++ mozy/week2/programmers/prob42586.py
@@ -2,28 +2,24 @@
 # 문제: 기능개발
 
 
-def minus(prog, speed):
-    return prog - speed if prog > speed else 0
-
-
-def pop(progresses, speeds):
-    count = 0
-    while progresses:
-        if progresses[0] != 0:
-            break
-        progresses.pop(0)
-        speeds.pop(0)
-        count += 1
-    return count
-
-
 def solution(progresses, speeds):
     answer = []
-    progress = [100 - x for x in progresses]
 
-    while progress:
-        for i in range(len(progress)):
-            progress[i] = minus(progress[i], speeds[i])
-        if progress[0] == 0:
-            answer.append(pop(progress, speeds))
+    # 중요!: Math.ceil 대신 -((progress - 100) // speed) 사용
+    progress_queue = [-((progress - 100) // speed) for progress, speed in zip(progresses, speeds)]
+    count = 0
+    while progress_queue:
+        if progress_queue[0] == 0:
+            progress_queue.pop(0)
+            count += 1
+        else:
+            if count > 0:
+                answer.append(count)
+                count = 0
+            progress_queue = [p - 1 if p != 0 else 0 for p in progress_queue]
+    if count > 0:
+        answer.append(count)
     return answer
+
+
+print(solution([76, 12, 66, 78, 9], [11, 45, 67, 88, 1]))
