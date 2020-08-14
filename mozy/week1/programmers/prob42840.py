2e265d50d3150aaba7b67cd3d0cc42a21ef47bd9
Mozy Lee <whyalwaysmeyy@gmail.com>
2020-08-14 23:55:20 +0900

    Refactor week1/programmers/prob42840

--- mozy/week1/programmers/prob42840.py
+++ mozy/week1/programmers/prob42840.py
@@ -3,23 +3,15 @@
 
 def solution(answers):
     scores = [0, 0, 0]
-    students = [[1, 3, 4, 5], [3, 1, 2, 4, 5]]
-    # student1: n%5+1, student2: n(짝: [1,3,4,5] 홀:2), student3...?
-    # 1: [1, 2, 3, 4, 5]
-    # 2: [2, 1, 2, 3, 2, 4, 2, 5]
-    # 3: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    students = [[1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
 
-    for i, answer in enumerate(answers):
-        if answer == i % 5 + 1:
-            scores[0] += 1
-        if i % 2 == 0:
-            if answer == 2:
-                scores[1] += 1
-        elif answer == students[0][int(i / 2) % 4]:
-            scores[1] += 1
-        if answer == students[1][int(i / 2) % 5]:
-            scores[2] += 1
-    return [i + 1 for i in range(3) if scores[i] == max(scores)]
+    for i, sol in enumerate(answers):
+        for j, ans in enumerate(students):
+            if sol == ans[i]:
+                scores[j] += 1
+
+    print(scores)
+    return [i + 1 for i, v in enumerate(scores) if v == max(scores)]
 
 
-solution([1, 2, 3, 4, 5])
+solution([1, 3, 2, 4, 2])
