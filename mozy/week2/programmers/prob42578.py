d63c77a207b291a89fdf485ceb0b8a4794894526
Mozy Lee <whyalwaysmeyy@gmail.com>
2020-08-18 01:11:54 +0900

    Refactor week2/programmers/prob42578__
    시간초과 뜸 ㅋㅋㅋ
    아마도 효울성 개선 조금만 하면 될듯

--- mozy/week2/programmers/prob42578.py
+++ mozy/week2/programmers/prob42578.py
@@ -1,19 +1,12 @@
 # 출처: programmers. https://programmers.co.kr/learn/courses/30/lessons/42578
 # 문제: 위장
 
-from math import factorial
-from collections import Counter
 from itertools import combinations
 
 
 def solution(clothes):
-    answer = 0
-    clothes_kind = [kind for cloth_name, kind in clothes]
-    kind = len(Counter(clothes_kind).keys())
-
-    for i in range(kind):
-        answer += len([x for x in combinations(clothes_kind, i + 1) if len(x) == len(set(x))])
-    return answer
+    cloth_kinds = [kind for cloth_name, kind in clothes]
+    return sum([len([x for x in combinations(cloth_kinds, i + 1) if len(x) == len(set(x))]) for i in range(len(set(cloth_kinds)))])
 
 
 print(solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]))
