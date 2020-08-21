d685a55dd6d172e912a2de9fcbd2f03cc6a8507c
Mozy Lee <whyalwaysmeyy@gmail.com>
2020-08-21 16:53:27 +0900

    Refactor week2/programmers/prob42578

--- mozy/week2/programmers/prob42578.py
+++ mozy/week2/programmers/prob42578.py
@@ -1,12 +1,12 @@
 # 출처: programmers. https://programmers.co.kr/learn/courses/30/lessons/42578
 # 문제: 위장
 
-from itertools import combinations
+from collections import Counter
+from functools import reduce
 
 
 def solution(clothes):
-    cloth_kinds = [kind for cloth_name, kind in clothes]
-    return sum([len([x for x in combinations(cloth_kinds, i + 1) if len(x) == len(set(x))]) for i in range(len(set(cloth_kinds)))])
+    return reduce(lambda x, y: x * (y + 1), Counter([kind for cloth, kind in clothes]).values(), 1) - 1
 
 
 print(solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]))
