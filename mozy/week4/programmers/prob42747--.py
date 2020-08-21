ad7374cf9e6244f2beb69205a607b1d0037f4032
Mozy Lee <whyalwaysmeyy@gmail.com>
2020-08-21 18:00:03 +0900

    Add unsolved week4/programmers/prob42747--
    지문이랑 예제보면서 이해가 안감.,..

---
+++ mozy/week4/programmers/prob42747--.py
@@ -0,0 +1,15 @@
+# 출처: programmers. https://programmers.co.kr/learn/courses/30/lessons/42747
+# 문제: H-Index
+
+# 처음엔 이분탐색으로 H-Index를 찾으려고 했으나 포기
+def solution(citations):
+    citations.sort()
+    length = len(citations)
+    print(citations)
+    for i, citation in enumerate(citations):
+        print(i, citation)
+        if i + 1 >= citation:
+            return length - (i + 1)
+
+
+print(solution([3, 0, 6, 1, 5]))
