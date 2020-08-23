5ebb7c097c57ac641b5439058f8f06699d12f9f4
Mozy Lee <whyalwaysmeyy@gmail.com>
2020-08-23 19:40:38 +0900

    Delete mozy directory

--- mozy/week1/boj/prob10799--.py
+++
@@ -1,4 +0,0 @@
-# Backjoon Online Judge: https://www.acmicpc.net/problem/10799
-# 문제: 쇠막대기
-
-# 못 풀..
--- mozy/week4/programmers/prob42748.py
+++
@@ -1,8 +0,0 @@
-# 출처: programmers. https://programmers.co.kr/learn/courses/30/lessons/42748
-# 문제: k번째 수
-
-def solution(array, commands):
-    answer = []
-    for command in commands:
-        answer.append(sorted(array[command[0] - 1:command[1]])[command[2] - 1])
-    return answer
