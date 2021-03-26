966c9057e96e046ecd5767023734cb790d1ede32
Mozy Lee <whyalwaysmeyy@gmail.com>
2021-03-26 17:57:17 +0900

    Add solved problems

---
+++ boj/prob11720.py
@@ -0,0 +1,9 @@
+# Backjoon Online Judge: https://www.acmicpc.net/problem/11720
+# 내용없음
+
+import sys
+
+_ = int(sys.stdin.readline())
+n = sys.stdin.readline().strip()
+
+print(sum(map(int, list(n))))
