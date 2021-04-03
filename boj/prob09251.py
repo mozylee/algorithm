67fd094951358870ed994f5713adedd804dc88ab
Mozy Lee <whyalwaysmeyy@gmail.com>
2021-04-03 14:06:08 +0900

    Add solved problems

---
+++ boj/prob09251.py
@@ -0,0 +1,16 @@
+# 출처: https://www.acmicpc.net/problem/9251
+# 문제: LCS
+
+
+a, b = input(), input()
+
+dp = [[0] * (len(b) + 1) for _ in range(len(a) + 1)]
+
+for i in range(1, len(a) + 1):
+    for j in range(1, len(b) + 1):
+        if a[i - 1] == b[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+
+print(dp[-1][-1])
