f9c0e580872df76801cbe3a754cd7e240e2177ce
Mozy Lee <whyalwaysmeyy@gmail.com>
2021-08-28 01:13:31 +0900

    Add solved problem boj/3046.java

---
+++ boj/prob03046.java
@@ -0,0 +1,14 @@
+package boj;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class prob03046 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int R1 = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        System.out.println(2 * S - R1);
+    }
+}
