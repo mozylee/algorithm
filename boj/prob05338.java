4cc66626ae672ab854a836da10f3ea7036e76ae4
Mozy Lee <whyalwaysmeyy@gmail.com>
2021-08-29 20:56:22 +0900

    Add solved problem boj/5338.java, 5339.java

---
+++ boj/prob05338.java
@@ -0,0 +1,7 @@
+package boj;
+
+public class prob05338 {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._\n'-..-'|   ||   |\n'-..-'|_.-;;-._|\n'-..-'|   ||   |\n'-..-'|_.-''-._|\n");
+    }
+}
