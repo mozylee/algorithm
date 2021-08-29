4852abaaad9abf769283bb268aa621a75413a9bf
Mozy Lee <whyalwaysmeyy@gmail.com>
2021-08-30 07:31:54 +0900

    Add solved problem boj/5522.java

---
+++ boj/prob05522.java
@@ -0,0 +1,14 @@
+package boj;
+
+import java.io.*;
+
+public class prob05522 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            sum += Integer.parseInt(br.readLine());
+        }
+        System.out.println(sum);
+    }
+}
