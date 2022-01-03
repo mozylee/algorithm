365352849e1bec59b8412ed4fbe4dbc11d30b6ac
mozy lee <whyalwaysmeyy@gmail.com>
2022-01-03 10:23:45 +0900

    Add solved problem boj/prob24183

--- boj/prob24183.py
+++ boj/prob24183.py
@@ -1,1 +1,7 @@
-a, b, c = map(int, input().split())
+C4, A3, A4 = map(int, input().split())
+
+C4_size = 0.229 * 0.324
+A3_size = 0.297 * 0.420
+A4_size = 0.210 * 0.297
+
+print(C4 * 2 * C4_size + A3 * 2 * A3_size + A4 * A4_size)
